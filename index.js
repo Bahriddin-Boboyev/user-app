834ccb71eb2b2318bdb621df96bc273188c869e0
scoderr <BahriddinBoboyev997@gmail.com>
2023-05-18 09:12:41 +0500

    users app update

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ import { Low } from "lowdb";
 import { JSONFile } from "lowdb/node";
 import { randomUUID } from "node:crypto";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+/* import Joi from "joi";
+
+const usersSchema = Joi.object({
+  firstName: Joi.string().required().min(1),
+  lastName: Joi.string().required().min(1),
+  email: Joi.string().required().email(),
+  password: Joi.string().required().min(6),
+});
+*/
 const app = express();
 app.use(express.json());
+dotenv.config();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const file = join(__dirname, "db.json");
 
@@ -18,6 +30,12 @@ const db = new Low(adapter, defaultData);
 app.post("/users/register", async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
+    await db.read();
+    const { users } = await db.data;
+    const usercheck = users.find((user) => user.email === email);
+    if (usercheck) {
+      return res.send("email already exists");
+    }
     const hashPassword = await bcrypt.hash(password, 10);
     await db.read();
     const user = {
@@ -43,10 +61,13 @@ app.post("/users/login", async (req, res) => {
     const { users } = await db.data;
     const user = users.find((user) => user.email === email);
     const checkPassword = await bcrypt.compare(password, user.password);
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
     if (user && checkPassword) {
       process.env.userID = user.id;
       console.log(process.env.userID);
-      return res.send(user);
+      return res.status(200).json({ token });
     } else {
       return res.send("user not found.");
     }
@@ -96,46 +117,58 @@ app.get("/users/me", async (req, res) => {
 });
 
 app.patch("/users/me", async (req, res) => {
-  await db.read();
-  let { users } = await db.data;
-  const user = await users.find((user) => user.id === process.env.userID);
-  let index = users.indexOf(user);
+  try {
+    if (process.env.userID) {
+      await db.read();
+      let { users } = await db.data;
+      const user = await users.find((user) => user.id === process.env.userID);
+      let index = users.indexOf(user);
 
-  users[index] = {
-    id: user.id,
-    firstName: req.body?.firstName ? req.body.firstName : user.firstName,
-    lastName: req.body?.lastName ? req.body.lastName : user.lastName,
-    email: req.body?.email ? req.body.email : user.email,
-    password: req.body?.password
-      ? await bcrypt.hash(req.body.password, 10)
-      : user.password,
-    role: user.role,
-  };
-  await db.write();
-  res.send(users);
+      users[index] = {
+        id: user.id,
+        firstName: req.body?.firstName ? req.body.firstName : user.firstName,
+        lastName: req.body?.lastName ? req.body.lastName : user.lastName,
+        email: req.body?.email ? req.body.email : user.email,
+        password: req.body?.password
+          ? await bcrypt.hash(req.body.password, 10)
+          : user.password,
+        role: user.role,
+      };
+      await db.write();
+      res.send(users);
+    } else {
+      res.send("You need to login");
+    }
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.post("/users", async (req, res) => {
   try {
-    await db.read();
-    let { users } = await db.data;
-    const user = await users.find((user) => user.id === process.env.userID);
-    if (user.role === "superAdmin") {
-      const { firstName, lastName, email, password } = req.body;
-      const hashPassword = await bcrypt.hash(password, 10);
-      const newUser = {
-        id: randomUUID(),
-        firstName,
-        lastName,
-        email,
-        password: hashPassword,
-        role: "admin",
-      };
-      await db.data.users.push(newUser);
-      await db.write();
-      res.send(`Now ${firstName} is the new admin`);
+    if (process.env.userID) {
+      await db.read();
+      let { users } = await db.data;
+      const user = await users.find((user) => user.id === process.env.userID);
+      if (user.role === "superAdmin") {
+        const { firstName, lastName, email, password } = req.body;
+        const hashPassword = await bcrypt.hash(password, 10);
+        const newUser = {
+          id: randomUUID(),
+          firstName,
+          lastName,
+          email,
+          password: hashPassword,
+          role: "admin",
+        };
+        await db.data.users.push(newUser);
+        await db.write();
+        res.send(`Now ${firstName} is the new admin`);
+      } else {
+        return res.send("You are not authorized to enter here");
+      }
     } else {
-      return res.send("You are not authorized to enter here");
+      res.send("You need to login");
     }
   } catch (error) {
     res.send(error);
